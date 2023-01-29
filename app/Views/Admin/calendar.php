fd5f32cc70b1b8c16461c8ee00f53fbecd2cc032
nikol <114404166+nicolesauquillo@users.noreply.github.com>
2023-01-29 19:57:46 +0800

    admin boss

--- app/Views/Admin/calendar.php
+++ app/Views/Admin/calendar.php
@@ -1,12 +1,106 @@
 <?= $this->include('admin/inc/top'); ?>
 
 <body class="hold-transition sidebar-mini layout-fixed">
+    
     <div class="wrapper">
-        <?= $this->include('admin/inc/navbar'); ?>
-        <?= $this->include('admin/inc/sidebar'); ?>
+        <?= $this->include('admin/inc/navbar'); ?>     
     </div>
+    <!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+<!-- Sidebar -->
+<div class="sidebar">
+  <!-- Sidebar user panel (optional) -->
+  <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+    <div class="image">
+    <i class="fa fa-user fa-lg" style="color: white; margin-top: 8px;"><img src="" class="img-circle elevation-2"></i>
+    </div>
+    <div class="info">
+      <a href="#" class="d-block">Miko Mandia</a>
+    </div>
+  </div>
+  <!-- SidebarSearch Form -->
+  <div class="form-inline">
+    <div class="input-group" data-widget="sidebar-search">
+      <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+      <div class="input-group-append">
+        <button class="btn btn-sidebar">
+          <i class="fas fa-search fa-fw"></i>
+        </button>
+      </div>
+    </div>
+  </div>
+
+  <!-- Sidebar Menu -->
+  <nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+      <!-- Add icons to the links using the .nav-icon class
+             with font-awesome or any other icon font library -->
+
+      <li class="nav-item">
+        <a href="index" class="nav-link">
+        <i class="nav-icon fas fa-chart-pie"></i>
+          <p>Sales and Graph</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="<?= site_url('menu') ?>" class="nav-link">
+        <i class="nav-icon fas fa-list"></i>
+          <p>
+            Menu
+          </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="<?= site_url('calendar') ?>" class="nav-link active" style="background-color: #cb8c58;">
+          <i class="nav-icon far fa-calendar"></i>
+          <p>
+            Calendar
+          </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="<?= site_url('inbox') ?>" class="nav-link">
+          <i class="nav-icon fas fa-table"></i>
+          <p>
+            Booking
+          </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="<?= site_url('orders') ?>" class="nav-link">
+          <i class="nav-icon fas fa-shopping-bag"></i>
+          <p>
+            Orders
+          </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="<?= site_url('contactus') ?>" class="nav-link">
+          <i class="nav-icon fas fa-users"></i>
+          <p>
+          Customer Service
+          </p>
+        </a>
+      </li>
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      </div>
+      <li class="nav-item">
+        <a href="<?= site_url('logout') ?>" class="nav-link">
+          <i class="nav-icon fas fa-power-off"></i>
+          <p>
+            Log Out
+          </p>
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <!-- /.sidebar-menu -->
+</div>
+<!-- /.sidebar -->
+</aside>
+    
         <div class="content-wrapper">
-           
                     <!-- Content Header (Page header) -->
                     <section class="content-header">
                         <div class="container-fluid">
@@ -16,7 +110,7 @@
                                 </div>
                                 <div class="col-sm-6">
                                     <ol class="breadcrumb float-sm-right">
-                                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                                        <li class="breadcrumb-item" ><a href="products" style="color: #cb8c58;">Home</a></li>
                                         <li class="breadcrumb-item active">Calendar</li>
                                     </ol>
                                 </div>
@@ -31,7 +125,7 @@
                                     <div class="sticky-top mb-3">
                                         <div class="card">
                                             <div class="card-header">
-                                                <h4 class="card-title">Draggable Events</h4>
+                                                <h4 class="card-title">Events</h4>
                                             </div>
                                             <div class="card-body">
                                                 <!-- the events -->
--- app/Views/Admin/contact.php
+++ app/Views/Admin/contact.php
@@ -3,8 +3,101 @@
 <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
         <?= $this->include('admin/inc/navbar'); ?>
-        <?= $this->include('admin/inc/sidebar'); ?>
     </div>
+        <!-- Main Sidebar Container -->
+<aside class="main-sidebar sidebar-dark-primary elevation-4">
+
+<!-- Sidebar -->
+<div class="sidebar">
+  <!-- Sidebar user panel (optional) -->
+  <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+    <div class="image">
+    <i class="fa fa-user fa-lg" style="color: white; margin-top: 8px;"><img src="" class="img-circle elevation-2"></i>
+    </div>
+    <div class="info">
+      <a href="#" class="d-block">Miko Mandia</a>
+    </div>
+  </div>
+  <!-- SidebarSearch Form -->
+  <div class="form-inline">
+    <div class="input-group" data-widget="sidebar-search">
+      <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+      <div class="input-group-append">
+        <button class="btn btn-sidebar">
+          <i class="fas fa-search fa-fw"></i>
+        </button>
+      </div>
+    </div>
+  </div>
+
+  <!-- Sidebar Menu -->
+  <nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+      <!-- Add icons to the links using the .nav-icon class
+             with font-awesome or any other icon font library -->
+
+      <li class="nav-item">
+        <a href="index" class="nav-link">
+        <i class="nav-icon fas fa-chart-pie"></i>
+          <p>Sales and Graph</p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="<?= site_url('menu') ?>" class="nav-link">
+        <i class="nav-icon fas fa-list"></i>
+          <p>
+            Menu
+          </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="<?= site_url('calendar') ?>" class="nav-link">
+          <i class="nav-icon far fa-calendar"></i>
+          <p>
+            Calendar
+          </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="<?= site_url('inbox') ?>" class="nav-link">
+          <i class="nav-icon fas fa-table"></i>
+          <p>
+            Booking
+          </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="<?= site_url('orders') ?>" class="nav-link">
+          <i class="nav-icon fas fa-shopping-bag"></i>
+          <p>
+            Orders
+          </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="<?= site_url('contactus') ?>" class="nav-link active" style="background-color: #cb8c58;">
+          <i class="nav-icon fas fa-users"></i>
+          <p>
+          Customer Service
+          </p>
+        </a>
+      </li>
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      </div>
+      <li class="nav-item">
+        <a href="<?= site_url('logout') ?>" class="nav-link">
+          <i class="nav-icon fas fa-power-off"></i>
+          <p>
+            Log Out
+          </p>
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <!-- /.sidebar-menu -->
+</div>
+<!-- /.sidebar -->
+</aside>
     <div class="content-wrapper">
     <section class="content-header">
       <div class="container-fluid">
@@ -14,7 +107,7 @@
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="products">Home</a></li>
+            <li class="breadcrumb-item" ><a href="products" style="color: #cb8c58;">Home</a></li>
               <li class="breadcrumb-item active">Customer Service Inbox Table</li>
             </ol>
           </div>
@@ -52,8 +145,8 @@
                     <td><?= $inbox['message']?></td>
                     <td>
                       <span>
-                      <a href="" class="btn-sm btn-primary"><i class="nav-icon fas fa-check"></i></a>
-                      <a href="" class="btn-sm btn-danger"><i class="nav-icon fas fa-trash"></i></a>
+                      <a href="" class="btn-sm btn" style="background-color: #cb8c58; color: white;"><i class="nav-icon fas fa-check"></i></a>
+                      <a href="" class="btn-sm btn" style="background-color: #cb8c58; color: #7E3517;"><i class="nav-icon fas fa-trash"></i></a>
                       </span>
                     </td>
                   </tr>
